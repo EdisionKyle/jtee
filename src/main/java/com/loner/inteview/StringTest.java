1f0260d7805c6b7d7eb1cf02a16ab633747b8916
tianpc0318 <tianpc0318@163.com>
2017-01-13 18:12:47 +0800

    ti

--- src/main/java/com/loner/inteview/StringTest.java
+++ src/main/java/com/loner/inteview/StringTest.java
@@ -11,6 +11,7 @@ public class StringTest {
 		int i = 10;
 		i += i -= i - i;
 		System.out.println(x);
+		System.out.println("--------分割线------");
 		/**
 		 * JDK1.6下返回false false，而1.7后返回false true
 		 * 见【面试/Tech/原理分析】
@@ -24,6 +25,16 @@ public class StringTest {
 		s3.intern();
 		String s4 = "11";
 		System.out.println(s3 == s4);
+		/*String s = new String("1");
+		String s2 = "1";
+		s.intern();
+		System.out.println(s == s2);
+
+		String s3 = new String("1") + new String("1");
+		s3.intern();
+		String s4 = "11";
+		System.out.println(s3 == s4);*/
+		
 		/**
 		 * 见【面试/Tech/金典题目】
 		 */
