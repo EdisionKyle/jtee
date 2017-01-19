b42bb6a4b56c5f9699fa76ac1dc13e380fa970b3
tianpc0318 <tianpc0318@163.com>
2017-01-19 18:10:01 +0800

    ee

---
+++ src/main/java/com/loner/base/lang/reflect/BeanInfoTest.java
@@ -0,0 +1,55 @@
+package com.loner.base.lang.reflect;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+/**
+ * 自省测试
+ * 
+ * @author pengcheng.tian
+ *
+ */
+public class BeanInfoTest {
+
+	public static void main(String[] args) {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserName("peida");
+		try {
+			System.out.println(PropertyUtils.getProperty(userInfo, "userName"));
+			BeanInfoUtil.getPropertyByIntrospector(userInfo, "userName");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/*public static void main(String[] args) {
+		UserInfo userInfo = new UserInfo();
+		try {
+			BeanUtils.setProperty(userInfo, "userName", "peida");
+
+			System.out.println("set userName:" + userInfo.getUserName());
+
+			System.out.println("get userName:" + BeanUtils.getProperty(userInfo, "userName"));
+
+			BeanUtils.setProperty(userInfo, "age", 18);
+			System.out.println("set age:" + userInfo.getAge());
+
+			System.out.println("get age:" + BeanUtils.getProperty(userInfo, "age"));
+
+			System.out.println("get userName type:" + BeanUtils.getProperty(userInfo, "userName").getClass().getName());
+			System.out.println("get age type:" + BeanUtils.getProperty(userInfo, "age").getClass().getName());
+
+			PropertyUtils.setProperty(userInfo, "age", 8);
+			System.out.println(PropertyUtils.getProperty(userInfo, "age"));
+
+			System.out.println(PropertyUtils.getProperty(userInfo, "age").getClass().getName());
+
+			PropertyUtils.setProperty(userInfo, "age", "8");
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	}*/
+
+}
--- src/main/java/com/loner/qrcode/CommonTest.java
+++ src/main/java/com/loner/qrcode/CommonTest.java
@@ -1,12 +1,23 @@
 package com.loner.qrcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CommonTest {
 
 	public static void main(String[] args) {
 		int a = 1;
 		a++;
 		System.out.println(a++);
-
+		List<String> list = new ArrayList<String>();
+		list.add("milesloner");
+		list.add("menghua");
+		list.add("whereis");
+		Set<String> set = new HashSet<String>();
+		set.addAll(list);
+		System.out.println(set);
 	}
 
 }
